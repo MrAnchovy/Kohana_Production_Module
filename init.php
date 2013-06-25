bc1f21b981a6c1b732f91de2488dc9220a409cc9
MrAnchovy <mr.anchovy@mranchovy.com>
2013-06-25 10:37:53 +0100

    Add init.php - although routes have been moved to bootstrap.php it might be needed later

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Initialisation for the kpro (Kohana Production) module
+ *
+ * Sets the routes to be handled by a kpro controller. Note that none of these
+ * routes set an action, this is done by Kpro_Controller::before().
+ *
+ * @package    kpro
+ * @category   core
+ * @author     [MrAnchovy](http://www.mranchovy.com)
+ * @copyright  Copyright © 2013 [MrAnchovy](http://www.mranchovy.com)
+ * @license    http://kohanaframework.org/license
+ * @link       https://github.com/MrAnchovy/Kohana_Production_Module
+**/
